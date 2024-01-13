8e5c89b491a6eed36dd4756f0416ec5dce84be2e
Kelvin Rugut <krugut36@gmail.com>
2024-01-13 12:03:26 +0300

    Pycode style

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     """BaseModel class defining attrs and methods for the whole project"""
     def __init__(self, *args, **kwargs):
@@ -14,13 +15,15 @@ class BaseModel:
             if '__class__' in kwargs:
                 del kwargs['__class__']
             for key, val in kwargs.items():
-               if key in ['created_at', 'updated_at']:
-                  if isinstance(val, str):
-                      try:
-                          val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
-                      except ValueError:
-                          val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S')
-               setattr(self, key, val)
+                if key in ['created_at', 'updated_at']:
+                    if isinstance(val, str):
+                        try:
+                            val = datetime.strptime(
+                                val, '%Y-%m-%dT%H:%M:%S.%f')
+                        except ValueError:
+                            val = datetime.strptime(
+                                val, '%Y-%m-%dT%H:%M:%S')
+                setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -34,14 +37,14 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the public instance attribute 'updated_at' with the current datetime
+        Updates the pub inst attrs
+        'updated_at' with the current datetime
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """return a dict object"""
-        
         dict_obj = self.__dict__.copy()
         dict_obj['__class__'] = self.__class__.__name__
         dict_obj['created_at'] = self.created_at.isoformat()
